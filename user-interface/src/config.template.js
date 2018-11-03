acd535db2f7febfbe9eb1079ace455eb4d0ffe56
Wojciech Gawronski <afronski@gmail.com>
2018-11-03 21:30:21 +0100

    Better description for the configuration templates.

--- user-interface/src/config.template.js
+++ user-interface/src/config.template.js
@@ -3,19 +3,19 @@
 
   window.ServerlessTelephonist = {
     AWS: {
-      UserPoolId: "<USER_POOL_ID>",
-      IdentityPoolId: "<IDENTITY_POOL_ID>",
-      ClientId: "<CLIENT_ID>",
-      LoginService: "cognito-idp.<AWS_REGION>.amazonaws.com/<USER_POOL_ID>",
-      Region: "<AWS_REGION>"
+      UserPoolId: "<Id of Amazon Cognito user pool>",
+      IdentityPoolId: "<Id of Amazon Cognito identity pool>",
+      ClientId: "<Id of Amazon Cognito client user here>",
+      LoginService: "cognito-idp.<AWS region>.amazonaws.com/<Id of Amazon Cognito user pool>",
+      Region: "<AWS region>"
     },
 
     API: {
-      GetOnCallOperatorDetailsURL: "https://<YOUR_CSHARP_API_NAME>.execute-api.<AWS_REGION>.amazonaws.com/production/operators/current/details",
-      TestOnCallNumberURL: "https://<YOUR_FSHARP_API_NAME>.execute-api.<AWS_REGION>.amazonaws.com/production/calls/test"
+      GetOnCallOperatorDetailsURL: "https://<Your C#-based Amazon API Gateway endpoint>.execute-api.<AWS region>.amazonaws.com/production/operators/current/details",
+      TestOnCallNumberURL: "https://<Your F#-based Amazon API Gateway endpoint>.execute-api.<AWS region>.amazonaws.com/production/calls/test"
     },
 
-    OnCallNumber: "<ON_CALL_NUMBER>"
+    OnCallNumber: "+48<Phone number that will be paged and called by external systems>"
   };
 
 } (window));
